06029d26d33fe96e7d492f2a394549a56f244fbe
Daniel <dsalvatori2008@gmail.com>
2023-03-09 13:25:48 +0000

    changes made

--- cypress/integration/e2e/calculator.cy.js
+++ cypress/integration/e2e/calculator.cy.js
@@ -27,13 +27,45 @@ describe("Calculator", () => {
     cy.get('.display').should('contain', '7')
   })
 
-  it('should as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)', () => {
+  it('should as expected for a range of numbers (negative)', () => {
     cy.get('#number2').click()
     cy.get('#operator-subtract').click()
     cy.get('#number6').click()
     cy.get('#operator-equals').click()
     cy.get('.display').should('contain', '-4')
   })
+
+  it('should as expected for a range of numbers (positive)', () => {
+    cy.get('#number6').click()
+    cy.get('#operator-subtract').click()
+    cy.get('#number2').click()
+    cy.get('#operator-equals').click()
+    cy.get('.display').should('contain', '4')
+  })
+
+  it('should as expected for a range of numbers (decimals)', () => {
+    cy.get('#number2').click()
+    cy.get('#decimal').click()
+    cy.get('#number2').click()
+    cy.get('#operator-subtract').click()
+    cy.get('#number1').click()
+    cy.get('#decimal').click()
+    cy.get('#number2').click()
+    cy.get('#operator-equals').click()
+    cy.get('.display').should('contain', '1')
+  })
+
+  it('should as expected for a range of numbers (large numbers)', () => {
+    cy.get('#number2').click()
+    cy.get('#number2').click()
+    cy.get('#number2').click()
+    cy.get('#operator-subtract').click()
+    cy.get('#number6').click()
+    cy.get('#number2').click()
+    cy.get('#operator-equals').click()
+    cy.get('.display').should('contain', '160')
+  })
+
 
   it('should be able to do exceptional circumstances', () => {
     cy.get('#number2').click()
